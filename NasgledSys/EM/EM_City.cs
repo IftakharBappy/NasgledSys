16363abbb74d3dccb946923deb0e0814160cd8a8
atiksarker2013 <atiour.islam@gmail.com>
2017-08-15 20:32:40 +0600

    Add City Crud Operation

---
+++ NasgledSys/EM/EM_City.cs
@@ -0,0 +1,35 @@
+﻿using NasgledSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.EM
+{
+    public static class EM_City
+    {
+        public static CityViewModel ConvertToModel(CityList ent)
+        {
+            CityViewModel bo = new CityViewModel();
+
+            bo.CityKey = ent.CityKey;
+            bo.CityName = ent.CityName;
+            bo.StateName = ent.StateList.StateName;
+            bo.StateCode = ent.StateCode;
+            bo.IsDelete = ent.IsDelete;
+
+            return bo;
+        }
+
+        public static CityList ConvertToEntity(CityViewModel bo)
+        {
+            CityList ent = new CityList();
+            //ent.CityKey = bo.CityKey;
+            ent.CityName = bo.CityName;
+            ent.StateCode = bo.StateCode;
+            ent.IsDelete = bo.IsDelete;
+
+            return ent;
+        }
+    }
+}
---
+++ NasgledSys/Models/CityViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.Models
+{
+    public class CityViewModel
+    {
+        public int CityKey { get; set; }
+        [Display(Name = "Name*")]
+        [Required(ErrorMessage = "Name is required")]
+        public string CityName { get; set; }
+        [Display(Name = "State*")]
+
+        public string StateName { get; set; }
+
+        public Nullable<int> StateCode { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public List<CityClass> CityList { get; set; }
+    }
+}
