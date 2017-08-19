db40010ece0e39f194146322cabe986274c721de
saimum <saimum720@gmail.com>
2017-08-19 13:12:19 -0700

    LightingSatisfactionFactor Module Full

---
+++ NasgledSys/DAL/MgtLightingSatisfactionFactor.cs
@@ -0,0 +1,85 @@
+﻿using NasgledSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.DAL
+{
+    public class MgtLightingSatisfactionFactor
+    {
+        private NasgledDBEntities db = new NasgledDBEntities();
+        public List<LightingSatisfactionFactorClass> ListAll()
+        {
+            List<LightingSatisfactionFactorClass> obj = new List<LightingSatisfactionFactorClass>();
+            var temp = (from x in db.LightingSatisfactionFactor
+                        where x.IsDelete == false
+                        select new LightingSatisfactionFactorClass
+                        {
+                            PKey = x.PKey,
+                            FactorName = x.FactorName,
+                            Description = x.Description,
+                            IsDelete = x.IsDelete
+                        }).OrderBy(m => m.FactorName);
+            obj = temp.ToList();
+            return obj;
+        }
+
+        public int Add(LightingSatisfactionFactorClass obj)
+        {
+            int i = 1;
+            try
+            {
+                LightingSatisfactionFactor model = new LightingSatisfactionFactor();
+                model.PKey = Guid.NewGuid();
+                model.FactorName = obj.FactorName;
+                model.Description = obj.Description;
+                model.IsDelete = false;
+                db.LightingSatisfactionFactor.Add(model);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+
+        public int Update(LightingSatisfactionFactorClass obj)
+        {
+            int i = 1;
+            try
+            {
+                LightingSatisfactionFactor model = db.LightingSatisfactionFactor.Find(obj.PKey);
+                model.FactorName = obj.FactorName;
+                model.Description = obj.Description;
+
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+
+        public int Delete(Guid ID)
+        {
+            int i = 1;
+            try
+            {
+                LightingSatisfactionFactor model = db.LightingSatisfactionFactor.Find(ID);
+                model.IsDelete = true;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+    }
+}
