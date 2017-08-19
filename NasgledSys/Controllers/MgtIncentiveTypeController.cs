3f38ebee23b5781a598f3f9be262fb39bfa06c0b
iftakharBappy <iftekharbappy@gmail.com>
2017-08-19 13:05:51 +0600

    IncentiveType Controller

---
+++ NasgledSys/Controllers/MgtIncentiveTypeController.cs
@@ -0,0 +1,55 @@
+﻿using NasgledSys.DAL;
+using NasgledSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NasgledSys.Controllers
+{
+    public class MgtIncentiveTypeController : Controller
+    {
+        // GET: MgtIncentiveType
+        private NasgledDBEntities db = new NasgledDBEntities();
+        private MgtIncentiveType manage = new MgtIncentiveType();
+        public ActionResult Index()
+        {
+            try
+            {
+                IncentiveTypeClass obj = new IncentiveTypeClass();
+                obj.IncentiveTypeList = new List<IncentiveTypeClass>();
+                obj.IncentiveTypeList = manage.ListAll();
+                return View(obj);
+            }
+            catch (Exception e)
+            {
+                return View("Error", new HandleErrorInfo(e, "Home", "Index"));
+            }
+
+        }
+        public JsonResult Add(IncentiveTypeClass IncentiveType)
+        {
+            return Json(manage.Add(IncentiveType), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(IncentiveTypeClass IncentiveType)
+        {
+            return Json(manage.Update(IncentiveType), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Delete(Guid ID)
+        {
+            return Json(manage.Delete(ID), JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NasgledSys/DAL/MgtIncentiveType.cs
@@ -0,0 +1,85 @@
+﻿using NasgledSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.DAL
+{
+    public class MgtIncentiveType
+    {
+        private NasgledDBEntities db = new NasgledDBEntities();
+        public List<IncentiveTypeClass> ListAll()
+        {
+            List<IncentiveTypeClass> obj = new List<IncentiveTypeClass>();
+            var temp = (from x in db.IncentiveType
+                        where x.IsDelete == false
+                        select new IncentiveTypeClass
+                        {
+                            PKey = x.PKey,
+                            Description = x.Description,
+                            TypeName = x.TypeName,
+                            IsDelete = x.IsDelete
+                        }).OrderBy(m => m.TypeName);
+            obj = temp.ToList();
+            return obj;
+        }
+
+        public int Add(IncentiveTypeClass obj)
+        {
+            int i = 1;
+            try
+            {
+                IncentiveType model = new IncentiveType();
+                model.PKey = Guid.NewGuid();
+                model.TypeName = obj.TypeName;
+                model.Description = obj.Description;
+                model.IsDelete = false;
+                db.IncentiveType.Add(model);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+
+        public int Update(IncentiveTypeClass obj)
+        {
+            int i = 1;
+            try
+            {
+                IncentiveType model = db.IncentiveType.Find(obj.PKey);
+                model.TypeName = obj.TypeName;
+                model.Description = obj.Description;
+
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+
+        public int Delete(Guid ID)
+        {
+            int i = 1;
+            try
+            {
+                IncentiveType model = db.IncentiveType.Find(ID);
+                model.IsDelete = true;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                i = 0;
+            }
+            return i;
+        }
+    }
+}
