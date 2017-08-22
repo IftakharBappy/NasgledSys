548b2af223df2c0c162fdcb08bc122f8aeb3d578
saimum <saimum720@gmail.com>
2017-08-21 17:03:08 -0700

    SalesReachSetup Module Full done

--- NasgledSys/Controllers/UtilityController.cs
+++ NasgledSys/Controllers/UtilityController.cs
@@ -237,6 +237,18 @@ namespace NasgledSys.Controllers
             result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
             return result;
         }
+        public ActionResult LoadSalesReachSetupData(Guid SelectID)
+        {
+            JsonResult result = new JsonResult();
+            SalesReachSetupViewModel obj = new SalesReachSetupViewModel();
+            SalesReachSetup m = db.SalesReachSetup.Find(SelectID);
+            obj.PKey = m.PKey;
+            obj.TypeName = m.TypeName;
+            obj.Description = m.Description;
+            result.Data = obj;
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+        }
 
         public ActionResult CheckUsernameCreate(string user)
         {
