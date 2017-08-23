24e4c43e9aba11e0a5f02842793221286719f5cb
iftakharBappy <iftekharbappy@gmail.com>
2017-08-23 20:56:30 +0600

    ClientContact

---
+++ NasgledSys/Controllers/MgtClientContactController.cs
@@ -0,0 +1,99 @@
+﻿using NasgledSys.DAL;
+using NasgledSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NasgledSys.Controllers
+{
+    public class MgtClientContactController : Controller
+    {
+        private MgtClientContact manage = new MgtClientContact();
+        private NasgledDBEntities db = new NasgledDBEntities();
+        // GET: MgtClientContact
+        public ActionResult Index()
+        {
+            try
+            {
+                ClientContactClass obj = new ClientContactClass();
+                obj.ClientContactList = new List<ClientContactClass>();
+                obj.ClientContactList = manage.ListAll();
+                
+                return View(obj);
+            }
+            catch (Exception e)
+            {
+                return View("Error", new HandleErrorInfo(e, "Home", "Index"));
+            }
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CityKey = new SelectList(db.CityList.Where(m => m.IsDelete == false).OrderBy(m => m.CityName), "CityKey", "CityName");
+            ViewBag.StateKey = new SelectList(db.StateList.OrderBy(m => m.StateName), "Pkey", "StateName");
+            ViewBag.ProfileKey = new SelectList(db.UserProfile.OrderBy(m => m.Email), "ProfileKey");
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create(ClientContactClass ClientContact)
+        {
+            return Json(manage.Create(ClientContact), JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Details(Guid? ContactKey)
+        {
+            if (ContactKey == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientContact ClientContact = db.ClientContact.Find(ContactKey);
+            if (ClientContact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityKey = new SelectList(db.CityList.Where(m => m.IsDelete == false && m.CityKey == ClientContact.CityKey).OrderBy(m => m.CityName), "CityKey", "CityName");
+            ViewBag.StateKey = new SelectList(db.StateList.Where(m => m.IsDelete == false && m.PKey == ClientContact.StateKey).OrderBy(m => m.StateName), "Pkey", "StateName");
+            ViewBag.ProfileKey = new SelectList(db.UserProfile.OrderBy(m => m.Email), "ProfileKey");
+            return View(ClientContact);
+        }
+        public ActionResult Edit(Guid? ContactKey)
+        {
+            if (ContactKey == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientContact ClientContact = db.ClientContact.Find(ContactKey);
+            if (ClientContact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityKey = new SelectList(db.CityList.Where(m => m.IsDelete == false && m.CityKey == ClientContact.CityKey).OrderBy(m => m.CityName), "CityKey", "CityName");
+            ViewBag.StateKey = new SelectList(db.StateList.Where(m => m.IsDelete == false && m.PKey == ClientContact.StateKey).OrderBy(m => m.StateName), "Pkey", "StateName");
+            ViewBag.ProfileKey = new SelectList(db.UserProfile.OrderBy(m => m.Email), "ProfileKey");
+            return View(ClientContact);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(ClientContact ClientContact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ClientContact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = ClientContact.ContactKey });
+            }
+            ViewBag.CityKey = new SelectList(db.CityList.Where(m => m.IsDelete == false && m.CityKey == ClientContact.CityKey).OrderBy(m => m.CityName), "CityKey", "CityName");
+            ViewBag.StateKey = new SelectList(db.StateList.Where(m => m.IsDelete == false && m.PKey == ClientContact.StateKey).OrderBy(m => m.StateName), "Pkey", "StateName");
+            ViewBag.ProfileKey = new SelectList(db.UserProfile.OrderBy(m => m.Email), "ProfileKey");
+            return View(ClientContact);
+        }
+    }
+}
---
+++ NasgledSys/Models/ClientContactClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.Models
+{
+    public class ClientContactClass
+    {
+        public System.Guid ContactKey { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string OfficePhone { get; set; }
+        public string Address { get; set; }
+        public Nullable<System.Guid> ProfileKey { get; set; }
+        public string JobTitle { get; set; }
+        public string WebAddress { get; set; }
+        public string CellPhone { get; set; }
+        public string FaxPhone { get; set; }
+        public string Address2 { get; set; }
+        public Nullable<int> CityKey { get; set; }
+        public Nullable<int> StateKey { get; set; }
+        public string Zipcode { get; set; }
+        public string InternalNote { get; set; }
+        public string GeneralNote { get; set; }
+        public Nullable<bool> IsDefault { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+
+        public virtual CityList CityList { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+        public virtual StateList StateList { get; set; }
+
+        public List<ClientContactClass> ClientContactList { get; set; }
+    }
+}
