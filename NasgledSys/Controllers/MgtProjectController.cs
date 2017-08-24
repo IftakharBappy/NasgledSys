17416f103a936ba747e3a5b3e9daf63ef3ba6953
nahidmustafa84 <nahid.ntiers@gmail.com>
2017-08-24 14:27:16 +0600

    Get Latest

--- NasgledSys/Controllers/MgtProjectController.cs
+++ NasgledSys/Controllers/MgtProjectController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using NasgledSys.Models;
+using System.Web.Security;
+using System.Web.Mail;
+using System.Threading.Tasks;
+using NasgledSys.EM;
+using NasgledSys.Validation;
 namespace NasgledSys.Controllers
 {
     public class MgtProjectController : Controller
     {
         // GET: MgtProject
+        private NasgledDBEntities db = new NasgledDBEntities();
+        private FormValidation validate = new FormValidation();
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                ViewBag.mess = "";               
+                return View();
+            }
+            catch (Exception e)
+            {
+
+                return View("Error", new HandleErrorInfo(e, "Home", "Userhome"));
+            }
+        }
+
+        public ActionResult Create(Guid? id)
+        {
+            try
+            {
+                Session["GlobalMessege"] = "";
+                ViewBag.mess = "";
+                return View();
+            }
+            catch (Exception e)
+            {
+
+                return View("Error", new HandleErrorInfo(e, "Home", "Userhome"));
+            }
         }
     }
 }
