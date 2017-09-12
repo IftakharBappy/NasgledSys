6e2dd166ab453d2cf1c0dcdf672c7201bf106501
iftakharBappy <iftekharbappy@gmail.com>
2017-09-12 20:24:58 +0600

    Unit (Edit , Delete)

--- NasgledSys/Controllers/MgtUnitController.cs
+++ NasgledSys/Controllers/MgtUnitController.cs
@@ -1,7 +1,9 @@
-﻿using NasgledSys.Models;
+﻿using NasgledSys.DAL;
+using NasgledSys.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -58,11 +60,13 @@ namespace NasgledSys.Controllers
                         Session["GlobalMessege"] = "Contact is Saved Successfully";
                         return RedirectToAction("Index");
                     }
+                    
                     return View(obj);
+
                 }
                 catch (Exception ex)
                 {
-                    return View("Error", new HandleErrorInfo(ex, "MgtClientContact", "Index"));
+                    return View("Error", new HandleErrorInfo(ex, "MgtUnit", "Index"));
                 }
             }
             else
@@ -71,5 +75,57 @@ namespace NasgledSys.Controllers
                 return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
             }
         }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(UnitViewModel obj)
+        {
+            if (GlobalClass.MasterSession)
+            {
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        Unit model = db.Unit.Find(obj.UnitKey);
+                        model.UnitName = obj.UnitName;
+                        model.UnitShortname = obj.UnitShortname;
+                        if (string.IsNullOrEmpty(obj.UnitName)) model.UnitName = obj.UnitShortname;
+                        db.SaveChanges();
+                        Session["GlobalMessege"] = "Unit Info is Updated";
+                        return RedirectToAction("Index", new { id = obj.UnitKey });
+                    }
+
+                    return View(obj);
+                }
+                catch (Exception ex)
+                {
+                    return View("Error", new HandleErrorInfo(ex, "MgtUnit", "Index"));
+                }
+            }
+            else
+            {
+                Exception e = new Exception("Session Expired");
+                return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
+            }
+        }
+
+        public JsonResult Delete(Guid? ID)
+        {
+            Unit model = db.Unit.Where(x => x.UnitKey == ID).FirstOrDefault();
+            db.Unit.Remove(model);
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
     }
 }
