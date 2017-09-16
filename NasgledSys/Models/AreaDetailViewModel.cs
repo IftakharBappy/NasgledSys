32fa4475a0addaed44837187b54ca2bed7a7289c
Md. Atiour Islam <atiour.islam@gmail.com>
2017-09-17 00:06:01 +0600

    Area Details Edit Update

--- NasgledSys/Models/AreaDetailViewModel.cs
+++ NasgledSys/Models/AreaDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +11,26 @@ namespace NasgledSys.Models
 
         public System.Guid DetailKey { get; set; }
         public Nullable<System.Guid> AreaKey { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "AverageIlluminnce")]
         public Nullable<decimal> AverageIlluminnce { get; set; }
+
+        [Display(Name = "LightingSatisfactionKey")]
         public Nullable<System.Guid> LightingSatisfactionKey { get; set; }
+
+        [Display(Name = "CeilingHeight")]
         public Nullable<decimal> CeilingHeight { get; set; }
+
+        [Display(Name = "Reflectance")]
         public Nullable<decimal> Reflectance { get; set; }
+
+        [Display(Name = "AreaWidth")]
         public Nullable<decimal> AreaWidth { get; set; }
+
+        [Display(Name = "Length")]
         public Nullable<decimal> Length { get; set; }
 
         //public virtual LightingSatisfactionFactor LightingSatisfactionFactor { get; set; }
