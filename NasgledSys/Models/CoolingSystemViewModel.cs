93b37c8c3c9cc3214f06e86f4d5e8ab3d57f5a42
iftakharBappy <iftekharbappy@gmail.com>
2017-09-21 17:07:13 +0600

    PROJECT - AUDIT - SCHEDULE (OperatingSchedule ,NewRateSchdule , HeatingSystem,CoolingSystem)

---
+++ NasgledSys/Models/CoolingSystemViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.Models
+{
+    public class CoolingSystemViewModel
+    {
+        public System.Guid CoolingSystemKey { get; set; }
+        public Nullable<System.Guid> ProjectKey { get; set; }
+        [Display(Name = " Name*")]
+        [Required(ErrorMessage = "Name is Required.")]
+        public string SystemName { get; set; }
+        [Display(Name = " Annual Run Time*")]
+        [Required(ErrorMessage = "Annual Run Time is Required.")]
+        public Nullable<decimal> AnnualRunTime { get; set; }
+        [Display(Name = " Cooling System Type*")]
+        [Required(ErrorMessage = "Cooling System Type is Required.")]
+        public Nullable<System.Guid> CoolingSystemTypeKey { get; set; }
+        [Display(Name = " Fuel Type*")]
+        [Required(ErrorMessage = "Fuel Type is Required.")]
+        public Nullable<System.Guid> FuelTypeKey { get; set; }
+        [Display(Name = " Fuel Cost*")]
+        [Required(ErrorMessage = "Fuel Cost is Required.")]
+        public Nullable<decimal> FuelCost { get; set; }
+        [Display(Name = " Efficiency Type*")]
+        [Required(ErrorMessage = "Efficiency Type is Required.")]
+        public Nullable<System.Guid> EfficiencyType { get; set; }
+        [Display(Name = " Efficiency Value*")]
+        [Required(ErrorMessage = "Efficiency Value is Required.")]
+        public Nullable<decimal> EfficiencyValue { get; set; }
+        [Display(Name = " Internal Note")]
+        public string InternalNote { get; set; }
+        [Display(Name = " General Note")]
+        public string GeneralNote { get; set; }
+        public Nullable<bool> IsDefault { get; set; }
+
+        public virtual CoolingEfficientyType CoolingEfficientyType { get; set; }
+        public virtual CoolingSystemType CoolingSystemType { get; set; }
+        public virtual FuelType FuelType { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
---
+++ NasgledSys/Models/ScheduleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NasgledSys.Models
+{
+    public class ScheduleViewModel
+    {
+        public System.Guid PKey { get; set; }
+        [Display(Name = " Name*")]
+        [Required(ErrorMessage = " Name is required.")]
+        public string OPName { get; set; }
+        public Nullable<System.Guid> ProjectKey { get; set; }
+        [Display(Name = "Annual Operation Hour*")]
+        [Required(ErrorMessage = "Annual Operation Hour is required.")]
+        public Nullable<decimal> AnnualOperationHour { get; set; }
+        [Display(Name = " Internal Note")]
+        public string InternalNote { get; set; }
+        [Display(Name = " General Note")]
+        public string GeneralNote { get; set; }
+        public Nullable<bool> IsDefault { get; set; }
+    }
+}
