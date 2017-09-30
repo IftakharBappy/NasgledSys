3306ea7a9fbd45e71d999558dc23ff714754c967
nahidmustafa84 <nahid.ntiers@gmail.com>
2017-09-30 10:31:40 +0600

    Area Cpy Complete

--- NasgledSys/Controllers/AreaCopyController.cs
+++ NasgledSys/Controllers/AreaCopyController.cs
@@ -29,6 +29,7 @@ namespace NasgledSys.Controllers
                 try
                 {
                     Guid pKey = id;
+                    #region Area Main
                     Area model = db.Area.Find(id);
                     NasgledDBEntities bc = new NasgledDBEntities();
                     Area obj = new Area();
@@ -114,7 +115,7 @@ namespace NasgledSys.Controllers
                         bc.SaveChanges();
                         bc = new NasgledDBEntities();
                     }
-
+                    #endregion
                     #region Area Product
                     var product = from x in db.AreaProduct where x.AreaKey == id && x.IsDelete == false select x;
                     if (product.Count() > 0)
@@ -242,6 +243,9 @@ namespace NasgledSys.Controllers
                         }
                     }
                     #endregion
+
+                    GoinAncestors(id, pKey);
+                    bc.Dispose();
                     return RedirectToAction("Edit", "MgtSubArea",new { id=pKey});
                 }
                 catch (Exception e)
@@ -255,6 +259,242 @@ namespace NasgledSys.Controllers
                 Exception e = new Exception("Session Expired");
                 return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
             }
+        }
+
+        public void GoinAncestors(Guid id,Guid Destiny)
+        {
+            NasgledDBEntities bc = new NasgledDBEntities();
+            try
+            {
+                var temp = from x in db.Area where x.IsDelete == false && x.ParentAreaKey == id select x;              
+                if (temp.Count() > 0)
+                {
+                    foreach (var ichinose in temp)
+                    {
+                        #region Area Main
+                        Guid pKey = Guid.Empty;
+
+                        Area obj = new Area();
+                        obj.AreaKey = Guid.NewGuid();
+                        obj.AreaName = "Copy of " + ichinose.AreaName;
+                        obj.Reception = "--";
+                        obj.SquareFeet = ichinose.SquareFeet;
+                        obj.OperationScheduleKey = ichinose.OperationScheduleKey;
+                        obj.NewRateScheduleKey = ichinose.NewRateScheduleKey;
+                        obj.CoolingSystemKey = ichinose.CoolingSystemKey;
+                        obj.HeatingSystemKey = ichinose.HeatingSystemKey;
+                        obj.ProjectKey = GlobalClass.Project.ProjectKey;
+                        obj.IsDelete = false;
+                        obj.ParentAreaKey = Destiny;
+                        bc.Area.Add(obj);
+                        bc.SaveChanges();
+                        pKey = obj.AreaKey;
+                        bc = new NasgledDBEntities();
+
+                        AreaDetail dt = db.AreaDetail.FirstOrDefault(m => m.AreaKey == ichinose.AreaKey);
+                        if (dt == null) { }
+                        else
+                        {
+                            AreaDetail detail = new AreaDetail();
+                            detail.DetailKey = Guid.NewGuid();
+                            detail.AreaKey = pKey;
+                            detail.Description = dt.Description;
+                            detail.AverageIlluminnce = dt.AverageIlluminnce;
+                            detail.LightingSatisfactionKey = dt.LightingSatisfactionKey;
+                            detail.CeilingHeight = dt.CeilingHeight;
+                            detail.Reflectance = dt.Reflectance;
+                            detail.AreaWidth = dt.AreaWidth;
+                            detail.Length = dt.Length;
+                            bc.AreaDetail.Add(detail);
+                            bc.SaveChanges();
+                            bc = new NasgledDBEntities();
+
+                        }
+                        var doc = from x in db.AreaDocument where x.AreaKey == ichinose.AreaKey select x;
+                        if (doc.Count() > 0)
+                        {
+                            foreach (var item in doc)
+                            {
+                                AreaDocument dm = new AreaDocument();
+                                dm.DocumentKey = Guid.NewGuid();
+                                dm.AreaKey = pKey;
+                                dm.Description = item.Description;
+                                dm.FileType = item.FileType;
+                                dm.FileContent = item.FileContent;
+                                dm.FileName = item.FileName;
+                                bc.AreaDocument.Add(dm);
+                                bc.SaveChanges();
+                                bc = new NasgledDBEntities();
+                            }
+                        }
+                        var photo = from x in db.AreaPhoto where x.AreaKey == ichinose.AreaKey select x;
+                        if (photo.Count() > 0)
+                        {
+                            foreach (var pitem in photo)
+                            {
+                                AreaPhoto dm = new AreaPhoto();
+                                dm.PhotoKey = Guid.NewGuid();
+                                dm.AreaKey = pKey;
+                                dm.Description = pitem.Description;
+                                dm.FileType = pitem.FileType;
+                                dm.FileContent = pitem.FileContent;
+                                dm.FileName = pitem.FileName;
+                                bc.AreaPhoto.Add(dm);
+                                bc.SaveChanges();
+                                bc = new NasgledDBEntities();
+                            }
+                        }
+                        AreaNote note = db.AreaNote.FirstOrDefault(m => m.AreaKey == ichinose.AreaKey);
+                        if (note == null) { }
+                        else
+                        {
+                            AreaNote nt = new AreaNote();
+                            nt.NoteKey = Guid.NewGuid();
+                            nt.AreaKey = pKey;
+                            nt.NoteContent = note.NoteContent;
+                            nt.Internal = note.Internal;
+                            bc.AreaNote.Add(nt);
+                            bc.SaveChanges();
+                            bc = new NasgledDBEntities();
+                        }
+                        #endregion
+                        #region Area Product
+                        var product = from x in db.AreaProduct where x.AreaKey == ichinose.AreaKey && x.IsDelete == false select x;
+                        if (product.Count() > 0)
+                        {
+                            foreach (var item in product)
+                            {
+                                AreaProduct pd = new AreaProduct();
+                                pd.ProductKey = Guid.NewGuid();
+                                pd.AreaKey = pKey;
+                                pd.FixtureKey = item.FixtureKey;
+                                pd.Count = item.Count;
+                                pd.Description = item.Description;
+                                pd.ForEveryMainQty = item.ForEveryMainQty;
+                                pd.ReplaceQty = item.ReplaceQty;
+                                pd.SolutionName = item.SolutionName;
+                                pd.IncentiveTypeKey = item.IncentiveTypeKey;
+                                pd.IncentiveAmount = item.IncentiveAmount;
+                                pd.IncentiveMaxAmount = item.IncentiveMaxAmount;
+                                pd.IncentiveMaxTypeKey = item.IncentiveMaxTypeKey;
+                                pd.ProductName = item.ProductName;
+                                pd.ModelNo = item.ModelNo;
+                                pd.WattPerProduct = item.WattPerProduct;
+                                pd.L70 = item.L70;
+                                pd.ThermalEfficency = item.ThermalEfficency;
+                                pd.CustomNotes = item.CustomNotes;
+                                pd.ProductCost = item.ProductCost;
+                                pd.MarkupPercentage = item.MarkupPercentage;
+                                pd.InstallationTime = item.InstallationTime;
+                                pd.InstallationCost = item.InstallationCost;
+                                pd.ShipingCost = item.ShipingCost;
+                                pd.MiscCost = item.MiscCost;
+                                pd.IsOn = item.IsOn;
+                                pd.ProjectKey = item.ProjectKey;
+                                pd.IsDelete = item.IsDelete;
+                                pd.ExistingProductName = item.ExistingProductName;
+                                bc.AreaProduct.Add(pd);
+                                bc.SaveChanges();
+                                bc = new NasgledDBEntities();
+
+                                AreaProductDetail pdd = db.AreaProductDetail.FirstOrDefault(m => m.ProductKey == item.ProductKey);
+                                if (pdd == null) { }
+                                else
+                                {
+                                    AreaProductDetail newPdd = new AreaProductDetail();
+                                    newPdd.DetailKey = Guid.NewGuid();
+                                    newPdd.AreaKey = pKey;
+                                    newPdd.FixtureKey = pdd.FixtureKey;
+                                    newPdd.ProductKey = pd.ProductKey;
+                                    newPdd.ExistingControl = pdd.ExistingControl;
+                                    newPdd.OperationScheduleKey = pdd.OperationScheduleKey;
+                                    newPdd.WorkingFixtureCount = pdd.WorkingFixtureCount;
+                                    newPdd.ReplacementCost = pdd.ReplacementCost;
+                                    newPdd.AnnualMaintenance = pdd.AnnualMaintenance;
+                                    newPdd.InstallationInTime = pdd.InstallationInTime;
+                                    newPdd.Location = pdd.Location;
+                                    newPdd.Year = pdd.Year;
+                                    newPdd.LightingSatisfactionKey = pdd.LightingSatisfactionKey;
+                                    newPdd.MountingHeight = pdd.MountingHeight;
+                                    bc.AreaProductDetail.Add(newPdd);
+                                    bc.SaveChanges();
+                                    bc = new NasgledDBEntities();
+                                }
+
+                                #region AreaProductDocument
+                                var pdoc = from x in db.AreaProductDocument where x.ProductKey == item.ProductKey select x;
+                                if (pdoc.Count() > 0)
+                                {
+                                    foreach (var pditem in pdoc)
+                                    {
+                                        AreaProductDocument newpdoc = new AreaProductDocument();
+                                        newpdoc.DocumentKey = Guid.NewGuid();
+                                        newpdoc.AreaKey = pKey;
+                                        newpdoc.FixtureKey = pditem.FixtureKey;
+                                        newpdoc.ProductKey = pd.ProductKey;
+                                        newpdoc.FileType = pditem.FileType;
+                                        newpdoc.FileName = pditem.FileName;
+                                        newpdoc.FileContent = pditem.FileContent;
+                                        newpdoc.Description = pditem.Description;
+                                        bc.AreaProductDocument.Add(newpdoc);
+                                        bc.SaveChanges();
+                                        bc = new NasgledDBEntities();
+                                    }
+                                }
+                                #endregion
+
+                                #region AreaProductPhoto
+                                var pphoto = from x in db.AreaProductPhoto where x.ProductKey == item.ProductKey select x;
+                                if (pphoto.Count() > 0)
+                                {
+                                    foreach (var phditem in pphoto)
+                                    {
+                                        AreaProductPhoto newpdoc = new AreaProductPhoto();
+                                        newpdoc.PhotoKey = Guid.NewGuid();
+                                        newpdoc.AreaKey = pKey;
+                                        newpdoc.FixtureKey = phditem.FixtureKey;
+                                        newpdoc.ProductKey = pd.ProductKey;
+                                        newpdoc.FileType = phditem.FileType;
+                                        newpdoc.FileName = phditem.FileName;
+                                        newpdoc.FileContent = phditem.FileContent;
+                                        newpdoc.Description = phditem.Description;
+                                        bc.AreaProductPhoto.Add(newpdoc);
+                                        bc.SaveChanges();
+                                        bc = new NasgledDBEntities();
+                                    }
+                                }
+                                #endregion
+
+                                AreaProductNote pdnote = db.AreaProductNote.FirstOrDefault(m => m.ProductKey == item.ProductKey);
+                                if (pdnote == null) { }
+                                else
+                                {
+                                    AreaProductNote newpdnote = new AreaProductNote();
+                                    newpdnote.NoteKey = Guid.NewGuid();
+                                    newpdnote.AreaKey = pKey;
+                                    newpdnote.FixtureKey = pdnote.FixtureKey;
+                                    newpdnote.ProductKey = pd.ProductKey;
+                                    newpdnote.Condition = pdnote.Condition;
+                                    newpdnote.InternalNotes = pdnote.InternalNotes;
+                                    newpdnote.GeneralNote = pdnote.GeneralNote;
+                                    newpdnote.Description = pdnote.Description;
+                                    bc.AreaProductNote.Add(newpdnote);
+                                    bc.SaveChanges();
+                                    bc = new NasgledDBEntities();
+                                }
+                            }
+                        }
+                        #endregion
+
+                        GoinAncestors(ichinose.AreaKey, pKey);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = ex.Message.ToString();
+            }
+            bc.Dispose();
         }
         protected override void Dispose(bool disposing)
         {
