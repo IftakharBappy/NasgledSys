8cffc1913d5cdee839a903fd09364e95aa3daf9b
saimum <saimum720@gmail.com>
2017-10-05 12:15:58 -0700

    Proposal Customize Done

---
+++ NasgledSys/Controllers/MgtProposalCustomizeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NasgledSys.Models;
+using System.Web.Security;
+using System.Web.Mail;
+using System.Threading.Tasks;
+using NasgledSys.EM;
+using NasgledSys.Validation;
+using System.Globalization;
+using NasgledSys.DAL;
+using DataTables.Mvc;
+using System.Linq.Dynamic;
+
+namespace NasgledSys.Controllers
+{
+    public class MgtProposalCustomizeController : Controller
+    {
+        private NasgledDBEntities db = new NasgledDBEntities();
+
+        public ActionResult Customize(Guid id)
+        {
+            if (GlobalClass.MasterSession)
+            {
+                try
+                {
+                    ClientCompany company = db.ClientCompany.Find(GlobalClass.Project.CompanyKey);
+                    Proposal proposal = db.Proposal.Find(id);
+                    ProposalToCustomizeViewModel viewModel = new ProposalToCustomizeViewModel();
+                    viewModel.ProposalKey = proposal.ProposalKey;
+                    viewModel.CompanyKey = company.ClientCompanyKey;
+                    viewModel.CompanyName = company.CompanyName;
+                    viewModel.ProposalName = proposal.ProposalName;
+                    viewModel.ProjectKey = proposal.ProjectKey;
+                    viewModel.PreparedByText = proposal.PreparedByText;
+                    viewModel.IntroductionText = proposal.IntroductionText;
+                    viewModel.Team = proposal.Team;
+                    viewModel.Legal = proposal.Legal;
+                    viewModel.Disclaimer = proposal.Disclaimer;
+                    viewModel.Reference = proposal.Reference;
+                    viewModel.Billing = proposal.Billing;
+                    viewModel.Summary = proposal.Summary;
+                    viewModel.Pic = proposal.Pic;
+                    viewModel.Picname = proposal.Picname;
+                    viewModel.PicType = proposal.PicType;
+
+                    return View(viewModel);
+                }
+                catch (Exception e)
+                {
+
+                    return View("Error", new HandleErrorInfo(e, "MgtProject", "Created"));
+                }
+            }
+            else
+            {
+                Exception e = new Exception("Session Expired");
+                return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
+            }
+        }
+        [HttpPost]
+        public ActionResult Customize([Bind(Exclude = "Pic")] ProposalToCustomizeViewModel viewModel, HttpPostedFileBase Pic)
+        {
+            if (GlobalClass.MasterSession)
+            {
+                try
+                {
+                    Proposal proposal = db.Proposal.Find(viewModel.ProposalKey);
+                    proposal.ProposalName = viewModel.ProposalName;
+                    proposal.ProjectKey= viewModel.ProjectKey;
+                    proposal.PreparedByText= viewModel.PreparedByText;
+                    proposal.IntroductionText= viewModel.IntroductionText;
+                    proposal.Team= viewModel.Team;
+                    proposal.Legal = viewModel.Legal;
+                    proposal.Disclaimer = viewModel.Disclaimer;
+                    proposal.Reference = viewModel.Reference;
+                    proposal.Billing= viewModel.Billing;
+                    proposal.Summary = viewModel.Summary;
+                    if (Pic != null)
+                    {
+                        byte[] imgBinaryData = new byte[Pic.ContentLength];
+                        int readresult = Pic.InputStream.Read(imgBinaryData, 0, Pic.ContentLength);
+                        proposal.Pic = imgBinaryData;
+                        proposal.Picname = Pic.FileName;
+                        proposal.PicType = Pic.ContentType;
+                    }
+                    db.SaveChanges();
+                    return RedirectToAction("Customize", new { id = viewModel.ProposalKey });
+                }
+                catch (Exception e)
+                {
+                    return View("Error", new HandleErrorInfo(e, "Home", "Userhome"));
+                }
+            }
+            else
+            {
+                Exception e = new Exception("Session Expired");
+                return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
+            }
+        }
+    }
+}
---
+++ NasgledSys/Models/ProposalToCustomizeViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace NasgledSys.Models
+{
+    public class ProposalToCustomizeViewModel
+    {
+        public System.Guid ProposalKey { get; set; }
+
+        [Display(Name = "Proposal Name")]
+        public string ProposalName { get; set; }
+        public Nullable<System.Guid> ProjectKey { get; set; }
+        public Nullable<System.Guid> CompanyKey { get; set; }//CompanyName
+        public string CompanyName { get; set; }
+
+
+        [Display(Name = "Intro")]
+        public string IntroductionText { get; set; }
+        public string Team { get; set; }
+        public string Legal { get; set; }
+        public string Disclaimer { get; set; }
+        public string Reference { get; set; }
+
+        [Display(Name = "Billing Notes")]
+        public string Billing { get; set; }
+
+        [Display(Name = "Summury Estimate Project Description")]
+        public string Summary { get; set; }
+
+        [Display(Name = "Prepared By User")]
+        public string PreparedByText { get; set; }
+
+        [Display(Name = "Cover Photo")]
+        public byte[] Pic { get; set; }
+        public string PicType { get; set; }
+        public string Picname { get; set; }
+    }
+}
