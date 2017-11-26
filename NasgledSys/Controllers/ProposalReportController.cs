0112c034f1ee1feb7948807d6cf4d0f5c0ed4b5e
Nahid <nahid.ntiers@gmail.com>
2017-11-26 11:39:46 +0600

    --

--- NasgledSys/Controllers/ProposalReportController.cs
+++ NasgledSys/Controllers/ProposalReportController.cs
@@ -11,30 +11,89 @@ namespace NasgledSys.Controllers
     {
         public ActionResult Report(Guid id)
         {
-            ProposalReportViewModel model = new ProposalReportViewModel();
-
-            model.ProposalKey = id;
-
-            ClientCompany company = db.ClientCompany.Find(GlobalClass.Project.CompanyKey);
-           
-           
-            model.CompanyKey = company.ClientCompanyKey;
-            model.CompanyName = company.CompanyName;
-          
-
-            IQueryable<ReportTemplate> query = db.ReportTemplate;
-
-            var data = query.Select(asset => new ReportTemplateModel()
+            if (GlobalClass.MasterSession)
             {
-                TemplateKey = asset.TemplateKey,
-                FactorName = asset.FactorName,
-                TLevel = asset.TLevel
-            }).ToList();
+                try
+                {
+                    ProposalReportViewModel model = new ProposalReportViewModel();
 
-            model.FromReportTemplateList = data;
-            model.ToReportTemplateList = new List<ReportTemplateModel>();
+                    model.ProposalKey = id;
 
-            return View(model);
+                    ClientCompany company = db.ClientCompany.Find(GlobalClass.Project.CompanyKey);
+
+
+                    model.CompanyKey = company.ClientCompanyKey;
+                    model.CompanyName = company.CompanyName;
+
+
+                    IQueryable<ReportTemplate> query = db.ReportTemplate;
+
+                    var data = query.Select(asset => new ReportTemplateModel()
+                    {
+                        TemplateKey = asset.TemplateKey,
+                        FactorName = asset.FactorName,
+                        TLevel = asset.TLevel
+                    }).ToList();
+
+                    model.FromReportTemplateList = data;
+                    model.ToReportTemplateList = new List<ReportTemplateModel>();
+
+                    return View(model);
+                }
+                catch (Exception e)
+                {
+
+                    return View("Error", new HandleErrorInfo(e, "MgtProject", "Created"));
+                }
+            }
+            else
+            {
+                Exception e = new Exception("Session Expired");
+                return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Report(ProposalReportViewModel model)
+        {
+
+            if (GlobalClass.MasterSession)
+            {
+                try
+                {
+                    ClientCompany company = db.ClientCompany.Find(GlobalClass.Project.CompanyKey);
+
+
+                    model.CompanyKey = company.ClientCompanyKey;
+                    model.CompanyName = company.CompanyName;
+
+
+                    IQueryable<ReportTemplate> query = db.ReportTemplate;
+
+                    var data = query.Select(asset => new ReportTemplateModel()
+                    {
+                        TemplateKey = asset.TemplateKey,
+                        FactorName = asset.FactorName,
+                        TLevel = asset.TLevel
+                    }).ToList();
+
+                    model.FromReportTemplateList = data;
+                    model.ToReportTemplateList = new List<ReportTemplateModel>();
+
+                    return View(model);
+                }
+                catch (Exception e)
+                {
+
+                    return View("Error", new HandleErrorInfo(e, "MgtProject", "Created"));
+                }
+            }
+            else
+            {
+                Exception e = new Exception("Session Expired");
+                return View("Error", new HandleErrorInfo(e, "Home", "UserLogin"));
+            }
         }
     }
 }
